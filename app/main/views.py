88a955b3d51253874bd9cf7e4e1450ba14054b40
Eve Ishimwe <eveishimwe1@gmail.com>
2019-09-14 19:07:09 +0200

    made changes.

--- app/main/views.py
+++ app/main/views.py
@@ -12,7 +12,7 @@ def index():
     sources = get_sources('business')
     sports_sources = get_sources('sports')
     technology_sources = get_sources('entertainment')
-    title = "News hey"
+    title = "News Highlight"
 
 
     return render_templates('index.html',title = title, sources = sources, sports_sources = sports_sources, technology_sources = technology_sources, entertainment_sources = entertainment_sources)
